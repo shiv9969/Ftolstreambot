a7158d26c80e6b793bd548e640a974187b68f3c6
Assaulter-Shiv <assaulter819@gmail.com>
2024-08-26 12:24:42 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -6,31 +6,31 @@ from dotenv import load_dotenv
 
 
 load_dotenv()
-bot_name = "Gojo Satoru"
-bisal_channel = "https://telegram.me/Filmy_Men"
-bisal_grp = "https://t.me/FmDiscusss"
+bot_name = "BoB Link Generator"
+bisal_channel = "https://telegram.me/BoB_Files1"
+bisal_grp = "https://t.me/REQUESTING_MOVIES_SERIES_GROUPS"
 
 class Var(object):
-    MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ''))
-    API_HASH = str(getenv('API_HASH', ''))
-    BOT_TOKEN = str(getenv('BOT_TOKEN' , ''))
-    name = str(getenv('name', 'bisal_file2link_bot'))
+        MULTI_CLIENT = False
+    API_ID = int(getenv('API_ID', '9301087'))
+    API_HASH = str(getenv('API_HASH', 'cbabdb3f23de6326352ef3ac26338d9c'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7405488298:AAEK-UoqAytlNVSYI9dHDD-dM073SIHyRlU'))
+    name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
-    PORT = int(getenv('PORT', '8080'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002049789558')) 
+    PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "YOUR ID").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", " 1525203313").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'biisal'))
+    APP_NAME = str(getenv('APP_NAME'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Assaulter_Shiv'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('APP_NAME')) #dont need to fill anything here
+        APP_NAME = str(getenv('APP_NAME'))
     
+   
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
@@ -39,8 +39,8 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'bisal_files')) 
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
-    BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Kissu:Kissimunda.888@cluster0.f8ssexm.mongodb.net/?retryWrites=true&w=majority'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'BoB_Files1')) 
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1002049789558")).split()))   
+    BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "-1002049789558")).split()))   
     
