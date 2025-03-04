23b2ed71db7704f01a8799d0edd67d5de82345f2
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-03-04 13:49:40 +0530

    Delete biisal/bot/plugins/image.py

--- biisal/bot/plugins/image.py
+++
@@ -1,9 +0,0 @@
-from telegraph import upload_file
-
-async def upload_to_telegraph(file_path):
-    """Uploads an image to Telegraph and returns the link."""
-    try:
-        response = upload_file(file_path)
-        return f"https://telegra.ph{response[0]}"
-    except Exception as e:
-        return f"Error: {e}"
