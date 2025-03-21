4affb49abce15e13be6664e9a951b0782fa7adb0
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-03-21 22:03:00 +0530

    Update __init__.py

--- biisal/__init__.py
+++ biisal/__init__.py
@@ -4,3 +4,7 @@
 import time
 StartTime = time.time()
 __version__ = 1.1
+
+from pyrogram import utils as pyroutils
+pyroutils.MIN_CHAT_ID = -999999999999
+pyroutils.MIN_CHANNEL_ID = -100999999999999
