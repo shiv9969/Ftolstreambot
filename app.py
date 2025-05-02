84a4896ef439a983900abea72637c466a780181b
shiv9969 <150778501+shiv9969@users.noreply.github.com>
2025-05-02 16:07:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request, send_file
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Bisal_Files'
 
-
-if __name__ == "__main__":
-    app.run()
+@app.route('/watch/<int:msg_id>/<filename>')
+def watch(msg_id, filename):
+    file_url = f"https://api.telegram.org/file/bot{os.environ['BOT_TOKEN']}/{msg_id}/{filename}"
+    return render_template('player.html', video_url=file_url, filename=filename)
